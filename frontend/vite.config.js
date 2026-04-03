15696af7f15b96ff5808c76ae46e936eb828f533
Sathvika139 <raapellisathwika@gmail.com>
2026-04-03 18:30:52 +0000

    fixx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "import.meta.env.VITE_API_BASE_URL",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
